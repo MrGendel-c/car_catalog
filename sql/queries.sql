e3e34dd8dd6ec446f2a37ae1691f5d520edf22d8
MrGendel <leoni202002@mail.ru>
2026-02-02 21:12:07 +0300

    feat: add Docker support with postgres and custom adminer

--- sql/queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    brand, 
-    model, 
-    year,
-    CASE
-        WHEN EXTRACT(YEAR FROM CURRENT_DATE) - year <= 3 THEN 'новый'
-        ELSE 'старый'
-    END AS "Категория"
-FROM cars;
